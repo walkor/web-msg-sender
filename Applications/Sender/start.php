3af87d0fa0ed7dfc4473d131574afca8cd9a43c7
walkor <worker-man@qq.com>
2015-03-17 22:14:34 +0800

    update core  to 3.0.8

--- Applications/Sender/start.php
+++ Applications/Sender/start.php
@@ -1,21 +1,26 @@
 <?php 
+use \Workerman\Worker;
 use \Workerman\WebServer;
 use \GatewayWorker\Gateway;
 use \GatewayWorker\BusinessWorker;
 
-// gateway
+// 自动加载类
+require_once __DIR__ . '/../../Workerman/Autoloader.php';
+
+// gateway 进程
 $gateway = new Gateway("Websocket://0.0.0.0:3232");
-
+// 名称，以便status时查看方便
 $gateway->name = 'SenderGateway';
-
+// 进程数，建议与cpu核数相同
 $gateway->count = 4;
-
+// 分布式部署时需要设置成内网ip
 $gateway->lanIp = '127.0.0.1';
-
+// 内部通讯起始端口，假如$gateway->count=4，起始端口为4000
+// 则一般会使用4001 4002 4003 4004 4个端口作为内部通讯端口 
 $gateway->startPort = 48000;
-
+// 心跳间隔
 $gateway->pingInterval = 10;
-
+// 心跳数据
 $gateway->pingData = '{"type":"ping"}';
 
 /* 
@@ -36,18 +41,24 @@ $gateway->onConnect = function($connection)
 }; 
 */
 
-
-// bussinessWorker
+// bussinessWorker 进程
 $worker = new BusinessWorker();
-
+// 名称
 $worker->name = 'SenderBusinessWorker';
-
+// 进程数
 $worker->count = 4;
 
 
 // WebServer
 $web = new WebServer("http://0.0.0.0:3333");
-
+// WebServer进程数
 $web->count = 2;
-
+// 设置域名与站点目录映射关系
 $web->addRoot('www.your_domain.com', __DIR__.'/Web');
+
+
+// 如果不是在根目录启动，则运行runAll方法
+if(!defined('GLOBAL_START'))
+{
+    Worker::runAll();
+}
