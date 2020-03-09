76ef908499bba69c2106bbfe66c9e498f3684a9e
walkor <walkor@users.noreply.github.com>
2020-03-09 19:10:03 +0800

    Update start_web.php

--- start_web.php
+++ start_web.php
@@ -1,15 +1,61 @@
 <?php
 use Workerman\Worker;
-use Workerman\WebServer;
-use Workerman\Lib\Timer;
-use PHPSocketIO\SocketIO;
+use Workerman\Protocols\Http\Request;
+use Workerman\Protocols\Http\Response;
+use Workerman\Connection\TcpConnection;
 
 include __DIR__ . '/vendor/autoload.php';
 
 // 启动一个webserver，用于吐html css js，方便展示
 // 这个webserver服务不是必须的，可以将这些html css js文件放到你的项目下用nginx或者apache跑
-$web = new WebServer('http://0.0.0.0:2123');
-$web->addRoot('localhost', __DIR__ . '/web');
+$web = new Worker('http://0.0.0.0:2123');
+$web->name = 'web';
+
+define('WEBROOT', __DIR__ . '/web');
+
+$web->onMessage = function (TcpConnection $connection, Request $request) {
+    $path = $request->path();
+    if ($path === '/') {
+        $connection->send(exec_php_file(WEBROOT.'/index.html'));
+        return;
+    }
+    $file = realpath(WEBROOT. $path);
+    if (false === $file) {
+        $connection->send(new Response(404, array(), '<h3>404 Not Found</h3>'));
+        return;
+    }
+    // Security check! Very important!!!
+    if (strpos($file, WEBROOT) !== 0) {
+        $connection->send(new Response(400));
+        return;
+    }
+    if (\pathinfo($file, PATHINFO_EXTENSION) === 'php') {
+        $connection->send(exec_php_file($file));
+        return;
+    }
+
+    if (!empty($if_modified_since = $request->header('if-modified-since'))) {
+        // Check 304.
+        $info = \stat($file);
+        $modified_time = $info ? \date('D, d M Y H:i:s', $info['mtime']) . ' ' . \date_default_timezone_get() : '';
+        if ($modified_time === $if_modified_since) {
+            $connection->send(new Response(304));
+            return;
+        }
+    }
+    $connection->send((new Response())->withFile($file));
+};
+
+function exec_php_file($file) {
+    \ob_start();
+    // Try to include php file.
+    try {
+        include $file;
+    } catch (\Exception $e) {
+        echo $e;
+    }
+    return \ob_get_clean();
+}
 
 if(!defined('GLOBAL_START'))
 {
